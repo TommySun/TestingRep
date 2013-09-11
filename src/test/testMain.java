5e2249b748c9e157eee60613552190106b30eae3
tom.sun <tom.sun@blah.com>
2013-09-11 11:21:17 +1000

    Try Conflict commit

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -12,10 +12,11 @@ public class testMain {
 			System.out.println("hello world! Again! Fred");
 			System.out.println("hello world! Again! Bob");
 			System.out.println("hello world! Again! George");
-			System.out.println("hello world! Again! Ted");
+			System.out.println("hello world! Again! Pebble");
 			System.out.println("hello world! Again! Marshall ");
 			System.out.println("hello world! Again! Willma");
 			System.out.println("hello world! Again! Barney");
+			System.out.println("GO HOME AFTER 2AM");
 	}
 
 }
